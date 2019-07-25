d2e803bdc4e350d341c289731291afbf463e68be
Ajlloyd <ajlloyd98@gmail.com>
2019-07-25 18:15:10 +0100

    GD3: error function finished

--- gradient_descent.py
+++ gradient_descent.py
@@ -1,24 +1,43 @@
 import numpy as np
+import traceback
 
+##################
 
 def gs_test(x1,x2):
     a = x1 + x2
     return a
 
+from sklearn.datasets import make_regression
+
+x,y = make_regression(n_samples=1000,n_features=20,n_informative=10,
+                      n_targets=1,random_state=12)
 
 
+#########################
 
-def compute_error(x,y,m,b):
-    pass
+def compute_error(x,y,weights):
+    y = y.reshape(-1,1)
+    n = len(x)
+
+    error = 0
+    for i in range(0,len(x)):
+        xi=x[i,:].reshape(-1,1)
+        yi=y[i,:]
+        xw = np.dot(weights.T,xi)
+        sq = (xw-yi)**2
+        error += sq
+    return float(np.sqrt(error / n))
 
 
 
 def run_gd(x,y,l_rate, n_iterations):
-    np.random.seed(42)
-    initial_m = np.random.rand()
-    initial_b = np.random.rand()
+    ones = np.ones((x.shape[0],1))
+    x = np.c_[x,ones]
+    initial_w=np.random.rand(x.shape[1],1)
+    print("GD starting at initial weights...")
+    print("Initial error = {}".format(compute_error(x,y,initial_w)))
+    print("Optimising...")
 
-    print("GD starting at m= {}, b= {}".format(initial_m,initial_b))
-    print("Initial error= {}".format(compute_error(x,y,initial_m,initial_b))
+    #compute_error(x,y,initial_m,initial_b)
 
-run_gd(1,3,4,5)
+run_gd(x,y,0.01,1000)
