3b00e295eb817f06650c6b1f82f260093f78e586
Ajlloyd <ajlloyd98@gmail.com>
2019-07-25 13:34:27 +0100

    GD1: began run and error function

--- gradient_descent.py
+++ gradient_descent.py
@@ -1,4 +1,24 @@
+import numpy as np
+
 
 def gs_test(x1,x2):
     a = x1 + x2
     return a
+
+
+
+
+
+
+
+
+
+
+def run_gd(x,y,l_rate, n_iterations):
+
+    np.random.seed(42)
+    initial_b = np.random.rand()
+    initial_m = np.random.rand()
+    print("GD starting at b= {}, m= {}".format(initial_b,initial_m))
+
+run_gd(1,3,4,5)
