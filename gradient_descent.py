873dad12416101b476b3d7446e438c4ed2112009
Ajlloyd <ajlloyd98@gmail.com>
2019-07-25 13:37:32 +0100

    GD2: compute error function

--- gradient_descent.py
+++ gradient_descent.py
@@ -8,17 +8,17 @@ def gs_test(x1,x2):
 
 
 
-
-
-
+def compute_error(x,y,m,b):
+    pass
 
 
 
 def run_gd(x,y,l_rate, n_iterations):
-
     np.random.seed(42)
-    initial_b = np.random.rand()
     initial_m = np.random.rand()
-    print("GD starting at b= {}, m= {}".format(initial_b,initial_m))
+    initial_b = np.random.rand()
+
+    print("GD starting at m= {}, b= {}".format(initial_m,initial_b))
+    print("Initial error= {}".format(compute_error(x,y,initial_m,initial_b))
 
 run_gd(1,3,4,5)
