8771bfe1d52f3d8a8510cd7840459cb22a55fd3c
Ajlloyd <ajlloyd98@gmail.com>
2019-07-28 13:29:44 +0100

    GD4: changed to OOP

--- gradient_descent.py
+++ gradient_descent.py
@@ -1,43 +1,32 @@
 import numpy as np
-import traceback
 
-##################
+np.random.seed(42)
 
-def gs_test(x1,x2):
-    a = x1 + x2
-    return a
+class lin_reg_GD():
+    def __init__(self,x,y,iter,l_rate):
+        self.x = np.c_[x,np.ones((x.shape[0],1))]
+        self.y = y.reshape(-1,1)
+        self.iter = iter
+        self.l_rate = l_rate
+        self.w = np.random.rand(self.x.shape[1],1)
+        self.instances = len(self.x)
+        self._compute_error(self.w)
+        self._opt_weights()
 
-from sklearn.datasets import make_regression
+    def _compute_error(self, w):
+        error = 0
+        for i in range(0,self.instances):
+            Xi = self.x[i,:].reshape(-1,1)
+            Yi = self.y[i,:]
+            Xwi = np.dot((self.w).T,Xi)
+            error += (Xwi-Yi)**2
+        return np.sqrt(error / self.instances)
 
-x,y = make_regression(n_samples=1000,n_features=20,n_informative=10,
-                      n_targets=1,random_state=12)
-
-
-#########################
-
-def compute_error(x,y,weights):
-    y = y.reshape(-1,1)
-    n = len(x)
-
-    error = 0
-    for i in range(0,len(x)):
-        xi=x[i,:].reshape(-1,1)
-        yi=y[i,:]
-        xw = np.dot(weights.T,xi)
-        sq = (xw-yi)**2
-        error += sq
-    return float(np.sqrt(error / n))
-
-
-
-def run_gd(x,y,l_rate, n_iterations):
-    ones = np.ones((x.shape[0],1))
-    x = np.c_[x,ones]
-    initial_w=np.random.rand(x.shape[1],1)
-    print("GD starting at initial weights...")
-    print("Initial error = {}".format(compute_error(x,y,initial_w)))
-    print("Optimising...")
-
-    #compute_error(x,y,initial_m,initial_b)
-
-run_gd(x,y,0.01,1000)
+    def _opt_weights(self):
+        for i in range(self.iter):
+            Xw = np.dot(self.x,self.w)
+            XwY = Xw - self.y
+            w_gradients = (2*(np.dot(self.x.T, XwY)))/self.instances
+            self.w = self.w - (self.l_rate * w_gradients)
+        final_ws = self.w
+        return final_ws, w_gradients.round(decimals=4)
--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-import gradient_descent as gs
+import gradient_descent as gd
+
+from sklearn.datasets import make_regression
+from sklearn.preprocessing import StandardScaler
+
+xs,ys = make_regression(n_samples=10000,n_features=1,n_informative=1,n_targets=1,random_state=12)
+sc = StandardScaler()
+scaled = sc.fit_transform(xs)
 
 
-ans = gs.gs_test(13.5,77)
-print(ans)
+
+
+opt = gd.lin_reg_GD(scaled,ys,1000,0.01)
+
+optimised = opt._opt_weights()
+print(optimised)
+print(opt._compute_error(optimised))
