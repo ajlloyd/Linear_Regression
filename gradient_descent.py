06f69c9b4856d0715e016d0c520588560e114622
Ajlloyd <ajlloyd98@gmail.com>
2019-07-29 22:09:47 +0100

    GD5: added methods for _coefs and _pred to BGD, added scaler too

--- gradient_descent.py
+++ gradient_descent.py
@@ -1,8 +1,10 @@
 import numpy as np
+from sklearn.base import BaseEstimator, TransformerMixin
 
 np.random.seed(42)
 
-class lin_reg_GD():
+##### BATCH-GRADIENT-DESCENT:
+class lin_reg_BGD():
     def __init__(self,x,y,iter,l_rate):
         self.x = np.c_[x,np.ones((x.shape[0],1))]
         self.y = y.reshape(-1,1)
@@ -29,4 +31,47 @@ class lin_reg_GD():
             w_gradients = (2*(np.dot(self.x.T, XwY)))/self.instances
             self.w = self.w - (self.l_rate * w_gradients)
         final_ws = self.w
-        return final_ws, w_gradients.round(decimals=4)
+        return final_ws.ravel(), w_gradients
+
+    def _coefficients(self):
+        return self._opt_weights()[0][:-1]
+
+    def _intercept(self):
+        return self._opt_weights()[0][-1]
+
+    def _gradients(self):
+        return self._opt_weights()[1]
+
+    def _predict(self, new_x):
+        opt_w = self._opt_weights()[0].reshape(-1,1)
+        new_x_ones = np.c_[new_x,np.ones((new_x.shape[0],1))]
+        return new_x_ones.dot(opt_w)
+
+    def _score(self, y_pred, y):
+        # sqrt((pred - actual)**2 / instances)
+        #return np.sqrt(((y_pred - y)**2) / self.instances)
+        error = 0
+        for i in range(0,self.instances):
+            YPi = y_pred[i,:].reshape(-1,1) #pred
+            Yi = self.y[i,:] #true
+
+            error += (YPi-Yi)**2
+        return (error / self.instances)
+
+
+
+
+
+
+
+##### STANDARD-SCALER:
+class scaler(BaseEstimator,TransformerMixin):
+    def __init__(self):
+        return None
+    def fit(self,x):
+        return self
+    def transform(self,x):
+        mu = np.mean(x)
+        sigma = np.std(x)
+        z = (x - mu)/sigma
+        return z
--- main.py
+++ main.py
@@ -1,17 +1,40 @@
 import gradient_descent as gd
-
 from sklearn.datasets import make_regression
 from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+import numpy as np
 
-xs,ys = make_regression(n_samples=10000,n_features=1,n_informative=1,n_targets=1,random_state=12)
-sc = StandardScaler()
-scaled = sc.fit_transform(xs)
+np.set_printoptions(suppress=True)
+
+xs,ys = make_regression(n_samples=10000,n_features=10,
+                        n_informative=10,n_targets=1,random_state=12)
+
+x_train, x_test, y_train, y_test = train_test_split(xs,ys,test_size=0.33,random_state=12)
 
 
+sc = gd.scaler()
+scaled = sc.fit_transform(x_train)
 
 
-opt = gd.lin_reg_GD(scaled,ys,1000,0.01)
+"""clf = LinearRegression()
+a = clf.fit(scaled,y_train)
+print(clf.coef_, clf.intercept_)"""
 
-optimised = opt._opt_weights()
-print(optimised)
-print(opt._compute_error(optimised))
+
+clf = gd.lin_reg_BGD(scaled,y_train,1000,0.01)
+
+"""coefs = clf._coefficients()
+print(coefs)
+
+ints = clf._intercept()
+print(ints)
+
+grads = clf._gradients()
+print(grads)"""
+
+y_pred = clf._predict(x_train)
+
+print(clf._score(y_pred,y_train))
+print(mean_squared_error(y_pred, y_train.reshape(-1,1)))
