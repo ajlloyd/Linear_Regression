217bfb3888ebbd6d58f3bca1c9049a1d8f5729d8
Ajlloyd <ajlloyd98@gmail.com>
2019-07-31 23:31:19 +0100

    GD7: SGD finished

--- gradient_descent.py
+++ gradient_descent.py
@@ -3,8 +3,11 @@ from sklearn.base import BaseEstimator, TransformerMixin
 
 np.random.seed(42)
 
-##### BATCH-GRADIENT-DESCENT:
-class lin_reg_BGD():
+##### BATCH-GRADIENT-DESCENT-REGRESSION:
+# -uses all training examples (slow)
+# -lower bias (gets closer to the minimum)
+# -cannot jump out of local minima
+class reg_BGD:
     def __init__(self,x,y,iter,l_rate):
         self.x = np.c_[x,np.ones((x.shape[0],1))]
         self.y = y.reshape(-1,1)
@@ -12,7 +15,6 @@ class lin_reg_BGD():
         self.l_rate = l_rate
         self.w = np.random.rand(self.x.shape[1],1)
         self.instances = len(self.x)
-        self._compute_error(self.w)
         self._opt_weights()
 
     def _opt_weights(self):
@@ -44,7 +46,63 @@ class lin_reg_BGD():
             YPi = y_pred[i,:].reshape(-1,1) #pred
             Yi = self.y[i,:] #true
             error += (YPi-Yi)**2
-        return (error / self.instances)
+        return np.sqrt(error / self.instances)
+
+
+
+
+
+
+##### STOCHASTIC-GRADIENT-DESCENT-REGRESSION:
+# -uses random training instances (fast)
+# -higher bias (not as close to the minimum)
+# -can jump out of local minima (due to random nature)
+class reg_SGD(reg_BGD):
+    def __init__(self,x,y,iter,l_rate=0.01):
+
+        reg_BGD.__init__(self,x,y,iter,l_rate)
+        self._opt_weights_SGD()
+
+    def _opt_weights_SGD(self):
+        for epoch in range(1,self.iter+1):
+            for i in range(self.instances):
+                rand_i = np.random.randint(self.instances)
+                Xi = self.x[rand_i:rand_i+1,:]
+                Yi = self.y[rand_i:rand_i+1,:]
+                Xiw = np.dot(Xi,self.w)
+                XiwY = Xiw - Yi
+                w_gradients = (2*(np.dot(Xi.T, XiwY)))
+                sched = (epoch*self.instances) + i
+                eta = self.l_rate*(sched + 50)**-1
+                self.w = self.w - (eta * w_gradients)
+        final_ws = self.w
+        return final_ws.ravel(), w_gradients.round(decimals=4)
+
+    def _coefficients(self):
+        return self._opt_weights_SGD()[0][:-1]
+
+    def _intercept(self):
+        return self._opt_weights_SGD()[0][-1]
+
+    def _gradients(self):
+        return self._opt_weights_SGD()[1]
+
+    def _predict(self, new_x):
+        opt_w = self._opt_weights_SGD()[0].reshape(-1,1)
+        new_x_ones = np.c_[new_x,np.ones((new_x.shape[0],1))]
+        return new_x_ones.dot(opt_w)
+
+    def _score(self, y_pred, y):
+        error = 0
+        for i in range(0,self.instances):
+            YPi = y_pred[i,:].reshape(-1,1) #pred
+            Yi = self.y[i,:] #true
+            error += (YPi-Yi)**2
+        return np.sqrt(error / self.instances)
+
+
+
+
 
 
 ##### STANDARD-SCALER:
--- main.py
+++ main.py
@@ -1,40 +1,28 @@
 import gradient_descent as gd
 from sklearn.datasets import make_regression
-from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 import numpy as np
 
 np.set_printoptions(suppress=True)
 
-xs,ys = make_regression(n_samples=10000,n_features=10,
-                        n_informative=10,n_targets=1,random_state=12)
-
+##### TEST-REGRESSION-DATA:
+xs,ys = make_regression(n_samples=10000,n_features=15,
+                        n_informative=5,n_targets=1,random_state=12)
 x_train, x_test, y_train, y_test = train_test_split(xs,ys,test_size=0.33,random_state=12)
 
-
+##### SCALER:
 sc = gd.scaler()
 scaled = sc.fit_transform(x_train)
 
+##### BATCH-GD:
+clf1 = gd.reg_BGD(scaled,y_train,1000,0.01)
+y_pred1 = clf1._predict(x_train)
+print(clf1._score(y_pred1,y_train))
+#print(np.sqrt(mean_squared_error(y_pred1,y_train)))
 
-"""clf = LinearRegression()
-a = clf.fit(scaled,y_train)
-print(clf.coef_, clf.intercept_)"""
-
-
-clf = gd.lin_reg_BGD(scaled,y_train,1000,0.01)
-
-"""coefs = clf._coefficients()
-print(coefs)
-
-ints = clf._intercept()
-print(ints)
-
-grads = clf._gradients()
-print(grads)"""
-
-y_pred = clf._predict(x_train)
-
-print(clf._score(y_pred,y_train))
-print(mean_squared_error(y_pred, y_train.reshape(-1,1)))
+##### STOCHASTIC-GD:
+clf2 = gd.reg_SGD(scaled,y_train,5,0.5)
+y_pred2 = clf2._predict(x_train)
+print(clf2._score(y_pred2,y_train))
+#print(np.sqrt(mean_squared_error(y_pred2,y_train)))
