477fcb0932ab992de5b0997c5e4bc0072178ba0e
Ajlloyd <ajlloyd98@gmail.com>
2019-08-02 16:40:29 +0100

    RM2: Ridge clf complete

--- gradient_descent.py
+++ gradient_descent.py
@@ -56,7 +56,7 @@ class reg_BGD:
 # -higher bias (not as close to the minimum)
 # -can jump out of local minima (due to random nature)
 class reg_SGD(reg_BGD):
-    def __init__(self,x,y,iter,l_rate=0.01):
+    def __init__(self,x,y,iter=5,l_rate=0.5):
 
         reg_BGD.__init__(self,x,y,iter,l_rate)
         self._opt_weights_SGD()
@@ -92,18 +92,72 @@ class reg_SGD(reg_BGD):
 
     def _score(self, y_pred, y):
         error = 0
+        y=y.reshape(-1,1)
+        y_pred = y_pred.reshape(-1,1)
+        print(y_pred.shape,y.shape)
         for i in range(0,self.instances):
-            YPi = y_pred[i,:].reshape(-1,1) #pred
+            YPi = y_pred[i,:] #pred
             Yi = self.y[i,:] #true
             error += (YPi-Yi)**2
-        return np.sqrt(error / self.instances)
+        return (error / self.instances)
 
 
 
 
 ##### RIDGE-REGRESSION:
-class ridge():
-    pass
+class ridge(reg_BGD):
+    def __init__(self,x,y,iter=5,l_rate=0.5,alpha=0):
+        self.x = np.c_[x,np.ones((x.shape[0],1))]
+        self.y = y.reshape(-1,1)
+        self.iter = iter
+        self.l_rate = l_rate
+        self.w = np.random.rand(self.x.shape[1],1)
+        self.instances = len(self.x)
+        self.alpha = alpha
+        self._opt_weights_SGD()
+
+    def _opt_weights_SGD(self):
+        for epoch in range(0,self.iter):
+            for i in range(self.instances):
+                rand_i = np.random.randint(self.instances)
+                Xi = self.x[rand_i:rand_i+1,:]
+                Yi = self.y[rand_i:rand_i+1,:]
+                Xiw = np.dot(Xi,self.w)
+                XiwY = Xiw - Yi
+                w_gradients = (2*(np.dot(Xi.T, XiwY))) + self.alpha*(2*(self.w))
+                sched = (epoch*self.instances) + i
+                eta = self.l_rate*(sched + 50)**-1
+                self.w = self.w - (eta * w_gradients)
+        final_ws = self.w
+        return final_ws.ravel(), w_gradients.round(decimals=4)
+
+    def _coefficients(self):
+        return self._opt_weights_SGD()[0][:-1]
+
+    def _intercept(self):
+        return self._opt_weights_SGD()[0][-1]
+
+    def _gradients(self):
+        return self._opt_weights_SGD()[1]
+
+    def _predict(self, new_x):
+        opt_w = self._opt_weights_SGD()[0].reshape(-1,1)
+        new_x_ones = np.c_[new_x,np.ones((new_x.shape[0],1))]
+        return new_x_ones.dot(opt_w)
+
+    def _score(self, y_pred, y):
+        error = 0
+        y=y.reshape(-1,1)
+        y_pred = y_pred.reshape(-1,1)
+        print(y_pred.shape,y.shape)
+        for i in range(0,self.instances):
+            YPi = y_pred[i,:] #pred
+            Yi = self.y[i,:] #true
+            error += (YPi-Yi)**2
+        return (error / self.instances)
+
+
+
 
 ##### LASSO-REGRESSION:
 class lasso():
--- main.py
+++ main.py
@@ -3,26 +3,66 @@ from sklearn.datasets import make_regression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 import numpy as np
+import matplotlib.pyplot as plt
 
 np.set_printoptions(suppress=True)
 
 ##### TEST-REGRESSION-DATA:
-xs,ys = make_regression(n_samples=10000,n_features=15,
-                        n_informative=5,n_targets=1,random_state=12)
+xs,ys = make_regression(n_samples=1000,n_features=1,
+                        n_informative=1,noise = 50, n_targets=1,random_state=12)
 x_train, x_test, y_train, y_test = train_test_split(xs,ys,test_size=0.33,random_state=12)
+
+
 
 ##### SCALER:
 sc = gd.scaler()
 scaled = sc.fit_transform(x_train)
+scaled_t = sc.fit_transform(x_test)
 
-##### BATCH-GD:
+##### BATCH-GD: ----------------------------------------------------------------
 clf1 = gd.reg_BGD(scaled,y_train,1000,0.01)
-y_pred1 = clf1._predict(x_train)
-print(clf1._score(y_pred1,y_train))
-#print(np.sqrt(mean_squared_error(y_pred1,y_train)))
 
-##### STOCHASTIC-GD:
-clf2 = gd.reg_SGD(scaled,y_train,5,0.5)
-y_pred2 = clf2._predict(x_train)
-print(clf2._score(y_pred2,y_train))
-#print(np.sqrt(mean_squared_error(y_pred2,y_train)))
+# train + score:
+y_pred1 = clf1._predict(scaled)
+print("BatchGD train:", mean_squared_error(y_pred1,y_train))
+# test + score:
+yt_pred1 = clf1._predict(scaled_t)
+print("BatchGD test:", mean_squared_error(yt_pred1,y_test))
+
+
+##### STOCHASTIC-GD: -----------------------------------------------------------
+clf2 = gd.reg_SGD(scaled,y_train,iter=2,l_rate=0.5)
+
+# train + score:
+y_pred2 = clf2._predict(scaled)
+print("StochasticGD train:", mean_squared_error(y_pred2,y_train))
+# test + score:
+yt_pred2 = clf2._predict(scaled_t)
+print("StochasticGD test:", mean_squared_error(yt_pred2,y_test))
+
+
+##### RIDGE-GD: ----------------------------------------------------------------
+clf3 = gd.ridge(scaled,y_train,iter=5, l_rate=0.5, alpha=-0.1)
+
+# train + score:
+y_pred3 = clf3._predict(scaled)
+print("Ridge train:", mean_squared_error(y_pred3,y_train))
+# test + score:
+yt_pred3 = clf3._predict(scaled_t)
+print("Ridge test:",mean_squared_error(yt_pred3,y_test))
+
+
+
+##### PLOTS: -------------------------------------------------------------------
+plt.plot(x_train, y_train, "r.")
+
+line_x = np.linspace(-3,3,100)
+clf1_ys = clf1._predict(line_x)
+clf2_ys = clf2._predict(line_x)
+clf3_ys = clf3._predict(line_x)
+
+#plt.plot(line_x,clf1_ys)
+plt.plot(line_x,clf2_ys, "b-")
+plt.plot(line_x,clf3_ys,"g-")
+
+plt.show()
